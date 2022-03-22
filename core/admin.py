48e7347877a3210a72858b9e8620c00d31a07549
kazi-akib-abdullah <q.aqib.abdullah@gmail.com>
2022-03-22 08:08:39 +0600

    Added features

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 
-from .models import User, Tag
+from .models import User, Ingredient, Tag
 
 
 # Register your models here.
@@ -28,3 +28,4 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Tag)
+admin.site.register(Ingredient)
